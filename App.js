1108180d31e0955ee7fa0a78f0293726235539e6
Aditya1113 <aditya26.mam@gmail.com>
2024-01-10 14:38:06 +0530

    state

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
 import Person from './Components/Person';
 
 export default function App() {
-  const list = [
-    {id: '1', name: 'John'},
-    {id: '2', name: 'Alex'},
-    {id: '3', name: 'David'},
-    {id: '4', name: 'Joey'},
-    {id: '5', name: 'Tom'}
-  ];
+  const [count, setCount] = useState(0);
+
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
 
   return (
-    <>
-      {/* <Person firstname="Peter" lastname="Parker" />
-      <Person firstname="Rose" lastname="Mary" /> */}
-      {/* <Person age={24} /> */}
-      <Person list = {list} />
-    </>
+    <SafeAreaView>
+      <Text style={styles.text}>{count}</Text>
+      <Button title="Update" color="red" onPress={clickHandler} />
+    </SafeAreaView>
   );
 }
 
